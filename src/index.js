8765d95c7a001d3988a99c6c6216c1186e9c7135
Artur <artur-fedyuk@mail.ru>
2018-02-24 17:53:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = function warmup(temperature) {
   // your implementation here
-  if ((temperature) && (temperature === 0)) return temperature * 1.8 + 32;
+  if ((temperature) || (temperature === 0)) return temperature * 1.8 + 32;
 };
